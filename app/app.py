fe01d10ff6ed7dced141869a9685921ad0fe4df0
armundle <armundle@gmail.com>
2016-04-23 22:05:46 -0500

    Add alred endpoint

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify, render_template, Response, request
 import traceback
 from athena import get_open_appts, book_appointment, get_booked_appointment, reset_appointment
+from athena import extract_time, format_12
 import json
 
 app = Flask(__name__)
-
-#timeline = json.loads('timeline_sample_1.json')
 
 
 def bad_request(body=None):
@@ -15,6 +14,38 @@ def bad_request(body=None):
         response = Response(body)
     response.headers['status'] = 400
     return response
+
+
+APPT = {
+        "summary": "Your OBGYN appointment with Dr. Bonnie Buttercup is scheduled for May 1st 2016 at 8:30 am.",
+        #"appointment_date": {
+            #"day": "monday",
+            #"date": "2016-05-01 8:30 AM"
+        #},
+        "week": 0,
+        "timeline": "pregnancy",
+        "appointment_name": "OBGYN Appointment",
+        #"appointment_id": "988639184",
+        "category": "Single",
+        "booking Date": {
+            "booking_id": "2821481491",
+            "booking_date": {
+                "day": "sunday",
+                "date": "04/23/2016"
+            }
+        },
+        "Address": [
+            {
+                "name": "Seton Northwest Hospital",
+                "department": "OBGYN",
+                "address_line1": "11113 Research Blvd",
+                "address_line2": "STE 400",
+                "city": "Austin",
+                "zip": "78759",
+                "phoneno": "512-442-4522"
+            }
+        ]
+    }
 
 
 def create_appointment():
@@ -34,6 +65,12 @@ def create_appointment():
 
     return get_open_appts(dates)
 
+@app.route('/alfred', methods=['GET'])
+def alfred():
+    with open("timeline_sample_1.json") as json_file:
+        json_data = json.load(json_file)
+        return jsonify(json_data)
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -47,11 +84,22 @@ def reset():
 
 @app.route('/book', methods=['GET'])
 def book():
-    return jsonify(result = book_appointment(create_appointment()[0]))
+    appt = book_appointment(create_appointment()[0])
+    date = appt['date']
+    time = appt['starttime']
+    print date
+    print time
+    return jsonify(result = appt)
 
 
 @app.route('/booked', methods=['GET'])
 def booked():
+    appt = get_booked_appointment()['appointments'][0]
+    print appt
+    date = appt['date']
+    time = appt['starttime']
+    print date
+    print time
     return jsonify(result = get_booked_appointment())
 
 
